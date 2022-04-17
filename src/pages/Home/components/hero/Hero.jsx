256ec5bf819456707b94d435fbbe21ec1ba14ea4
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-17 16:50:41 +0530

    feat: added navigate for hero

--- src/pages/Home/components/hero/Hero.jsx
+++ src/pages/Home/components/hero/Hero.jsx
@@ -1,10 +1,18 @@
 import style from "./Hero.module.css";
+import { useNavigate } from "react-router-dom";
+import { useFilter } from "../../../../context";
 const Hero = () => {
+  const { filterDispatch: dispatch } = useFilter();
+  const navigate = useNavigate();
+  const navigateToProducts = () => {
+    dispatch({ type: "RESET" });
+    navigate("/products");
+  };
   return (
     <div className="main">
       <main>
         <section className={`${style.hero_wrapper} position-relative`}>
-          <div className={style.hero}>
+          <div className={style.hero} onClick={navigateToProducts}>
             <img
               src="https://res.cloudinary.com/clutchaf/image/upload/v1648459257/ClutchStore/Warriors/Banner/1969_NBA_TLP_Aspot_032122_WARR_Desk_dbbmsv.webp"
               alt=""
