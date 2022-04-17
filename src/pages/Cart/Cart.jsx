de0cc9ec03b5305b03ad155b79d5d7ab6566e713
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-17 21:30:50 +0530

    feat: added cart component

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,23 @@
-const Cart = () => {
+import { useCartAndWishlist } from "../../context";
+import style from "./Cart.module.css";
+import { CartProducts, CartSummary } from "./components";
+function Cart() {
+  const {
+    cartAndWishlistItems: { cart },
+  } = useCartAndWishlist();
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="main min-75-vh width-100 display-flex justify-content-center">
+      <main className="width-100">
+        {cart.length !== 0 && (
+          <div
+            className={`${style.cart_wrapper} display-grid mx-auto my-15 width-80 gap-1`}>
+            <CartProducts />
+            <CartSummary />
+          </div>
+        )}
+      </main>
     </div>
   );
-};
+}
 
 export { Cart };
