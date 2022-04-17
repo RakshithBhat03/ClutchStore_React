933d5beaadbd8e37f0fd705afabb6a7e5f13bf02
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-17 21:28:49 +0530

    chore: updated exports

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,2 +1,4 @@
 export { productsReducer } from "./productsReducer";
 export { filterReducer } from "./filterReducer";
+export { cartAndWishlistReducer } from "./cartAndWishlistReducer";
+export { cartSubtotalReducer } from "./cartSubtotalReducer";
