3458a6628e2eef4154133f00fb2119a4d8514ae8
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-17 12:26:03 +0530

    feat: footer and navigation

---
+++ src/components/nav/Navigation.jsx
@@ -0,0 +1,60 @@
+import "./Navigation.css";
+import { Link } from "react-router-dom";
+const Navigation = () => {
+  return (
+    <header className="navigation-wrapper">
+      <div className="navigation max-width-1200 mx-auto">
+        <div className="navigation__title">
+          <Link to="/">
+            <img
+              src="./assets/ClutchLogo.PNG"
+              alt="ClutchStore logo"
+              className="img--responsive nav--logo"
+            />
+          </Link>
+        </div>
+        <div className="navigation__search">
+          <div className="navigation__search-icon">
+            <i className="fas fa-search" />
+          </div>
+          <input type="text" className="input-field input--lg" />
+        </div>
+        <div className="navigation__login">
+          <Link to="/login">
+            <button className="btn btn-auth btn--md px-12">Login</button>
+          </Link>
+        </div>
+        <div className="navigation__hamburger">
+          <button className="btn btn--md btn--icon btn--outline">
+            <i className="fas fa-bars" />
+          </button>
+        </div>
+        <div className="navigation__menu">
+          <ul className="navigation__menu-list">
+            <li>
+              <Link to="/wishlist">
+                <button className="btn btn--md btn--icon btn--outline">
+                  <div className="badge-icon badge-icon--sm">
+                    <i className="fas fa-heart nav-icon" />
+                    <span className="badge badge--sm badge--top-right"></span>
+                  </div>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart">
+                <button className="btn btn--md btn--icon btn--outline">
+                  <div className="badge-icon badge-icon--sm">
+                    <i className="fas fa-shopping-cart nav-icon" />
+                    <span className="badge badge--sm badge--top-right"></span>
+                  </div>
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+export { Navigation };
