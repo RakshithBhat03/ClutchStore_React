bd1cc502162e4dec6d784e9dcee39378e7b2c4c7
RakshithBhat03 <rakshith6666@gmail.com>
2022-07-20 00:35:50 +0530

    [feat]: Added private routes

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,15 +1,29 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, Products, Login, Cart, Wishlist, Signup } from "../pages";
+import {
+  Home,
+  Products,
+  Login,
+  Cart,
+  Wishlist,
+  Signup,
+  SingleProduct,
+} from "../pages";
 import Mockman from "mockman-js";
+import { RequireAuth } from "./RequireAuth";
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/wishlist" element={<Wishlist />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/products/:id" element={<SingleProduct />} />
+      <Route element={<RequireAuth isLoggedIn />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+      <Route element={<RequireAuth />}>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Route>
       <Route path="/mock" element={<Mockman />} />
       <Route
         path="*"
