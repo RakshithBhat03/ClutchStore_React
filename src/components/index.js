af093a175c45e8e6e356154653552885b4f5d1fa
RakshithBhat03 <rakshith6666@gmail.com>
2022-07-20 00:33:37 +0530

    [chore]: updated exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 export { Navigation } from "./nav/Navigation";
 export { Footer } from "./footer/Footer";
+export { FormError } from "./FormError/FormError";
+export { ShowToast } from "./ShowToast/ShowToast";
+export { Loader } from "./Loader/Loader";
+export { WishlistButton } from "./button/WishlistButton";
+export { ProductCard } from "./card/ProductCard";
